21167abc0e88afef15f0cea760bc4c161a098002
tei4 <ishikawa.t.847@ms.saitama-u.ac.jp>
2024-12-04 02:50:02 +0900

    add CarPositionCheck.cs

---
+++ Assets/Scripts/CarPositionCheck.cs
@@ -0,0 +1,24 @@
+using System;
+using UnityEngine;
+
+public class CarPositionCheck: MonoBehaviour
+{
+    [SerializeField] private GameObject car;
+    [SerializeField] private GameObject stopLine;
+    private float _relativePos;
+    private readonly float _carHeadLength; // length between center of car and head of car
+
+    void Start()
+    {
+        // this code should attach stop line prefab object
+        stopLine = gameObject;
+        car = GameObject.Find("Car"); // or directly attach in inspector of stop line
+    }
+    
+    // call it if the car stops
+    public float CheckCarPos()
+    {
+        _relativePos = stopLine.transform.position.x - car.transform.position.x - _carHeadLength;
+        return _relativePos;
+    }
+}
