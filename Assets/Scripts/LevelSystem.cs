d91d642e1c24dcf39eccd17a4e9f0e6145a43fb0
yushin-H <y.hara.358@ms.saitama-u.ac.jp>
2024-12-20 19:23:26 +0900

    setting 60fps

--- Assets/Scripts/LevelSystem.cs
+++ Assets/Scripts/LevelSystem.cs
@@ -2,13 +2,13 @@ using UnityEngine;
 
 public class LevelSystem : MonoBehaviour
 {
-    public float [,]speedLevel = new float[4,5]{{0,0,0,50,50},{1,50,1000,50,30},{2,70,5000,70,50},{3,100,10000,90,60}};
+    public float [,]speedLevel = new float[4,5]{{0,0,0,50,50},{1,80,1000,50,30},{2,60,5000,70,50},{3,80,10000,90,60}};
     //{ゲーム内のレベル, 車の最大速度, レベルアップに必要なスコア, 信号が来る確率, 信号が来る時赤になる確率}(数値は仮)
     [SerializeField]public float maxSpeed;
     [SerializeField] public int currentLevel;
     void Start()
     {
-
+        Application.targetFrameRate = 60;
     }
 
     void Update()
