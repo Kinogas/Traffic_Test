e4aff6e1c2c7750c9db189603ed4452679318179
yushin-H <y.hara.358@ms.saitama-u.ac.jp>
2024-12-25 12:41:41 +0900

    update UI

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -3,6 +3,8 @@ using UnityEditor;
 using UnityEditor.Callbacks;
 using UnityEngine;
 using UnityEngine.Serialization;
+using TMPro;
+using UnityEngine.UI;
 
 public class CarController : MonoBehaviour
 {
@@ -11,11 +13,21 @@ public class CarController : MonoBehaviour
     public float currentSpeed;
     [SerializeField] private GameOverManager gameOverManager;  // GameOverManagerからゲームオーバーかどうかを教えてもらう
 
+    public GameObject highScore_object = null;
+    public GameObject score_object_2 = null;
+
+    public int highScore;
+    public int myScore;
+
     void Update()
     {
         LevelSystem levelSystem;
         GameObject obj = GameObject.Find("LevelSystem");
         levelSystem = obj.GetComponent<LevelSystem>();
+
+        ScoreCalc scoreCalc;
+        GameObject score = GameObject.Find("ScoreCalculator");
+        scoreCalc = score.GetComponent<ScoreCalc>();
 
         Rigidbody rb = this.GetComponent<Rigidbody> ();  // rigidbodyを取得
         Vector3 force = new Vector3 (0.0f,0.0f,speed);    // 力を設定
@@ -52,6 +64,25 @@ public class CarController : MonoBehaviour
         else
         {
             rb.linearVelocity = Vector3.zero;
+        }
+
+        if (gameOverManager.isGameOver)
+        {
+            myScore = (int)scoreCalc.GetCurrentScore();
+            highScore = PlayerPrefs.GetInt("SCORE", 0);
+            if (highScore < myScore)  //ハイスコアを超えた場合に更新
+            {
+            highScore = myScore;
+
+        　　　//"SCORE"をキーとして、ハイスコアを保存
+            PlayerPrefs.SetInt("SCORE", highScore);
+            PlayerPrefs.Save();//ディスクへの書き込み
+            }
+            Text highScore_text = highScore_object.GetComponent<Text>();
+            highScore_text.text = "ハイスコア：" + highScore;
+            Text score_text_2 = score_object_2.GetComponent<Text>();
+            score_text_2.text = " ス コ ア ：" + (int)scoreCalc.GetCurrentScore();
+            //Debug.Log("ハイスコア" + highScore);
         }
 
     }
--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -6,7 +6,13 @@ public class UI : MonoBehaviour
 {
     public GameObject speed_object = null;
     public GameObject level_object = null;
-    public GameObject score_object = null;
+    public GameObject score_object_1 = null;
+    //public GameObject score_object_2 = null;
+    //public GameObject highScore_object = null;
+
+    public int highScore;
+    public int myScore;
+    private GameOverManager gameOverManager;
     void Start()
     {
 
@@ -30,9 +36,31 @@ public class UI : MonoBehaviour
         speed_text.text = "スピード：" + (int)carController.currentSpeed;
 
         Text level_text = level_object.GetComponent<Text>();
-        level_text.text = "レベル：" + (int)levelSystem.currentLevel;
+        level_text.text = " レ ベ ル ：" + (int)levelSystem.currentLevel;
 
-        Text score_text = score_object.GetComponent<Text>();
-        score_text.text = "スコア：" + (int)scoreCalc.GetCurrentScore();
+        Text score_text_1 = score_object_1.GetComponent<Text>();
+        score_text_1.text = " ス コ ア ：" + (int)scoreCalc.GetCurrentScore();
+
+        /*Text score_text_2 = score_object_2.GetComponent<Text>();
+        score_text_2.text = " ス コ ア ：" + (int)scoreCalc.GetCurrentScore();*/
+
+        
+
+        /*if (gameOverManager.isGameOver)
+        {
+            myScore = (int)scoreCalc.GetCurrentScore();
+            highScore = PlayerPrefs.GetInt("SCORE", 0);
+            if (highScore < myScore)  //ハイスコアを超えた場合に更新
+            {
+            highScore = myScore;
+
+        　　　//"SCORE"をキーとして、ハイスコアを保存
+            PlayerPrefs.SetInt("SCORE", highScore);
+            PlayerPrefs.Save();//ディスクへの書き込み
+            }
+            Text highScore_text = highScore_object.GetComponent<Text>();
+            highScore_text.text = "ハイスコア：" + highScore;
+            Debug.Log("ハイスコア" + highScore);
+        }*/
     }
 }
